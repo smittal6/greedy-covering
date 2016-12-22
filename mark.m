cc3eed1b794800e30296136e9b1310a091384b66
Siddharth Mittal <smittal6@gmail.com>
2016-12-22 11:25:21 +0530

    Marking function

---
+++ mark.m
@@ -0,0 +1,27 @@
+#mark function.
+#separating the mode from markall.
+
+function mark(a,b,c)
+global allt;
+global mcount;
+global k=5; 
+global v=2;
+global t=3;
+sz=size(allt);
+initial=1;
+final=sz(1,1);
+while(initial<=final)
+  mid=floor((initial+final)/2);
+	if (allt(mid,1)==a)&&(allt(mid,2)==b)&&(allt(mid,3)==c)	
+    allt(mid,4)=1;
+    mcount++;
+    return;
+  else
+			if (a>allt(mid,1))||((a==allt(mid,1))&&(b>allt(mid,2)))||((a==allt(mid,1))&&(b==allt(mid,2))&&(c>allt(mid,3)))
+				initial=mid+1;
+			else
+				final=mid-1;
+      end
+  endif
+endwhile
+endfunction
